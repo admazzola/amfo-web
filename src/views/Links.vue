637d354c811a99f1ade62f3fdef1d48f702b60e3
Andy <admazzola@gmail.com>
2022-01-28 14:03:20 -0500

    adding links

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -33,55 +33,31 @@
   <ColorBars />
 
 
-    <div ref="aboutPanel"  class=" md:absolute ghostly   " style="left: 250px;top: 250px ;z-index:10" id="borders">
+    <div ref="panel"  class=" md:absolute   " style="left: 250px;top: 250px ;z-index:10; width:75%" id="borders">
 
        <div class="border-4 border-black p-2 p " style="width:90%;  ">
-         <div class="h-full p-4 text-xl font-bold grid md:grid-cols-2 " style="background: #aad3d9ee;   ">
+         <div class="h-full p-4 text-xl font-bold   " style="background: #aad3d9ee;   ">
            
-           <div class="inline-block  " >
+           <div class="block text-3xl font-bold  text-pink-500 p-4" style=" text-shadow: 2px 2px #ddddddcc;" >
 
-                <div class="p-2 border-4 border-white inline-block  margin-center" style="width:49%"  >
-                   <img src="@/assets/images/Mackenzie.jpg" class=" " width="100%" /> 
-                </div>
-
+              Links to my collections
 
             </div>
 
-           <div class="inline-block p-4 border-4 border-white">
-           Mackenzie O'Hara is an artist with high-functioning autism.  She graduated from art college with honors.  She currently works full time but in her free time she falls into her creative space where retro-tropical design comes to light!  Please stop in and take a peek at what fabulous things you might find.  Welcome to my art world! Please invite friends! 
+           <div class=" ">
 
+
+              <div v-for="row in linksArray" :key="row.title" class=" my-2"> 
+                
+                <a :href="row.url" target="_blank" class="inline-block hover:bg-pink-200 p-4 border-4 border-white " > {{row.title}} </a> 
+              </div>
            
           </div>
 
 
         </div>
 
-         <div class="h-full p-4 text-xl font-bold  " style="background: #aad3d9ee;   ">
-          
-          <div class="grid md:grid-cols-2 p-4 ">
-            <div></div> 
-
-            <div class="border-4 border-white p-4 ">
-                  <div class=" " >
-                    I specialize in: 
-                  </div>
-
-                  <ul class="p-4">
-                    <li> Abstract </li>
-                    <li> Acrylics </li>
-                    <li> Watercolor </li>
-                    <li> Furniture Painting </li>
-                    <li> Canvases </li>
-                    <li> Mixed Media (Fabric, etc.) </li>
-                    <li> Digital Art </li>
-                    <li> Autodesk Sketchbook </li>          
-                  
-                  </ul>
-              </div> 
-
-              </div> 
-
-        </div>
+       
 
 
        </div>
@@ -118,13 +94,18 @@ export default {
   components: {Navbar,Decorations,LogoContainer,SideNav,ColorBars,Footer},
   data() {
     return {
-       
+      linksArray: [
+        {title:'Redbubble Store', url: 'https://www.redbubble.com/people/amforium'},
+        {title:'Facebook Page', url: 'https://www.facebook.com/mackenzie.ohara.5'}
+
+
+      ]
 
     }
   },
   mounted() {
 
-     const el = this.$refs.aboutPanel;
+     const el = this.$refs.panel;
 
     if (el) {
       // Use el.scrollIntoView() to instantly scroll to the element
