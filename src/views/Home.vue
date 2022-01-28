1af868a6cbeb5ee98479042b4d4e78d050fac748
Andy <admazzola@gmail.com>
2022-01-28 01:06:36 -0500

    readjust

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,18 +22,18 @@
    <div class="   ghostly " style="left:0;top:0; z-index:0;overflow:hidden; " id="decorations">
 
 
-    <div class="absolute bg-tree-1" style="overflow-hidden;left:6rem; top:14rem; width:600px; height:900px" > 
+    <div class="absolute bg-tree-1" style="overflow-hidden;left:5%; top:14rem; width:500px; height:900px" > 
             
         </div>
 
 
-    <div class="absolute bg-tree-2" style="overflow-hidden;left:26rem; top:8rem; width:700px; height:900px" > 
+    <div class="absolute bg-tree-2" style="overflow-hidden;left:26%; top:8rem; width:600px; height:900px" > 
             
         </div>
 
 
  
-       <img src="@/assets/images/amfo_weeds.png" width="200px" class="absolute" style="left:18rem;top:180px" > 
+       <img src="@/assets/images/amfo_weeds.png" width="200px" class="absolute" style="left:16rem;top:200px" > 
 
       
       
