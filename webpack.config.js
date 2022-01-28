7a8c611710d5024e5152a0217b208f93b692d078
Andy <admazzola@gmail.com>
2022-01-28 13:36:57 -0500

    installing tailwind again

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // ...
+    module: {
+      rules: [
+        {
+          // ...
+          use: [
+            // ...
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  ident: 'postcss',
+                  plugins: [
+                    require('tailwindcss'),
+                    require('autoprefixer'),
+                  ],
+                },
+              }
+            },
+          ],
+        }
+      ],
+    }
+  }
