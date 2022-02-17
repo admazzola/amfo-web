5e73eed9025bf8ec6be84c776a24dbb4d808f7c8
Andy <admazzola@gmail.com>
2022-02-17 13:50:42 -0500

    fix width

--- src/views/components/LogoContainer.vue
+++ src/views/components/LogoContainer.vue
@@ -1,11 +1,11 @@
 <template>
 
-   <div class="border-4 border-black p-2 pl-16 " style="width:60rem; height:240px">
+   <div class="border-4 border-black p-2 pl-16 " style="width:100%; height:240px">
    
    
    <a class=" " style=" " href="/#/">
 
-       <div class=" pt-4 pl-24 " style="width:60rem; height:240px">
+       <div class=" pt-4 pl-24 " style="width:100%; height:240px">
             <img src="@/assets/images/amfo_logo.png" width="140px" /> 
        </div>
 
